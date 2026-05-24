c4abe26a1345e87f7ac60704439123c03f25a7d0
Rafeef2Rizq <rfyfrzq7@gmail.com>
2026-05-24 12:05:06 +0300

    publication  clean

--- index.py
+++ index.py
@@ -17,3 +17,14 @@ filtered=filtered.sort_values(by="joined_year",ascending=True)
 letters=filtered["last_name"].str[0]
 word="".join(letters)
 print(word)
+
+#publication cleaning
+df_json=pd.read_json('Session_data/publications.json')
+# print(df_json.head())
+# print(df_json.info())
+# print(df_json.isnull().sum())
+# print(df_json.duplicated().sum())
+
+#cleaning publication
+top_result = df_json.loc[df_json["citations"].idxmax()]
+print(top_result)
